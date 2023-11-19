d157ffef0f20c2813f43fa6adcf611269aad2dc1
Alexey T <rostovboy@gmail.com>
2023-11-19 16:46:29 +0300

    Created entity Article and migration

--- README.md
+++ README.md
@@ -85,5 +85,36 @@ where DATABASE_URL="mysql://user:password@db:3306/database_name?serverVersion=5.
 
 get database credentials from /docker/.env
 
+++++++++++++++++++++++++++++++++++++++++++++
+
+create first entity
+
+docker exec -it symfony_docker-php-fpm /bin/sh
+
+php bin/console list make
+
+php bin/console make:entity
+
+Article
+
+- title - string - 255 - no
+- introtext - string - 255 - no
+- content - text - no
+- published - boolean - no
+- slug - string - 150 - no
+
+https://www.doctrine-project.org/projects/doctrine-dbal/en/3.7/reference/types.html
+
+When you're ready, create a migration with:
+
+php bin/console make:migration
+
+this command will make migration file in /migrations directory
+
+Review the new migration then run it with:
+
+php bin/console doctrine:migrations:migrate
+
+this creates two tables in our database: articles and doctrine_migration_versions
 
 
---
+++ src/Entity/Article.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $introtext = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?bool $published = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $slug = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getIntrotext(): ?string
+    {
+        return $this->introtext;
+    }
+
+    public function setIntrotext(string $introtext): static
+    {
+        $this->introtext = $introtext;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function isPublished(): ?bool
+    {
+        return $this->published;
+    }
+
+    public function setPublished(bool $published): static
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
